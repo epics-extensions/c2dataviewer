e7558548b49154ed6ae923f58ce7cf6206e631b4
Guobao Shen <gshen@anl.gov>
2019-01-30 13:30:44 -0600

    fix the path for warning ui file

--- c2dataviewer/imagev.py
+++ c2dataviewer/imagev.py
@@ -72,7 +72,8 @@ def imagev(pv, label, scale=None, noAGC=True):
             super(LimitDialog, self).__init__(parent=parent)
             self.setupUi(self)
 
-    warning_class = uic.loadUiType("c2dataviewer/ui/warning.ui")[0]
+    warning_path = os.path.join(os.path.dirname(__file__), "ui/warning.ui")[0]
+    warning_class = uic.loadUiType(warning_path)[0]
 
     class WarningDialog(QtWidgets.QDialog, warning_class):
         def __init__(self, parent=None):
