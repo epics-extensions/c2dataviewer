5b7221b7357987b2e9b90515527aa247b70a8066
Matic Pogacnik <mpogacnik@anl.gov>
2020-09-09 10:55:28 +0200

    Redesign the imagev screen, to follow layout principles

--- test/test_imagev.py
+++ test/test_imagev.py
@@ -15,6 +15,7 @@ from PyQt5.QtWidgets import QWidget
 
 from pyqtgraph.Qt import QtWidgets
 from c2dataviewer.imagev import ImageWindow
+from PyQt5.QtCore import QPoint
 
 
 app = QtWidgets.QApplication(sys.argv)
@@ -28,32 +29,64 @@ class TestImagev(unittest.TestCase):
         """
         Test if widgets start to disappear if window
         is resized to smallest possible size.
+
+        As this gets really complex because of the relative coordinates,
+        map everything to global (screen) coordinates and test in global space
+
         """
 
         # Get main window handle
         mainWindow = self.window
 
-        def testChildVisibility():
-            # Get parent geometry
-            mainWindowGeometry = mainWindow.geometry()
+        # Get all child widgets
+        widgets = mainWindow.findChildren(QWidget)
 
-            # Get all child widgets
-            widgets = mainWindow.findChildren(QWidget)
+        def pointOnMainWindow(point):
+
+            # Get main window global coordinates
+            mainWindowGeometry = mainWindow.geometry()
+            mainWindowTopLeft = mainWindow.mapToGlobal(QPoint(0, 0))
+            mainWindowTopRight = mainWindow.mapToGlobal(QPoint(mainWindowGeometry.width(), 0))
+            mainWindowBottomLeft = mainWindow.mapToGlobal(QPoint(0, mainWindowGeometry.height()))
+            #mainWindowBottomRight = mainWindow.mapToGlobal(QPoint(mainWindowGeometry.width(), mainWindowGeometry.height()))
+
+            if (point.x() >= mainWindowTopLeft.x() and
+                point.x() <= mainWindowTopRight.x() and
+                point.y() >= mainWindowTopLeft.y() and
+                point.y() <= mainWindowBottomLeft.y()):
+                return True
+            else :
+                return False
+
+        def testWidgetsIfOnMainScreen():
 
             # Loop over them and see if they are all contained in a main window
             for _, w in enumerate(widgets):
-                self.assertTrue(mainWindowGeometry.contains(w.geometry()), f"Size check failed at: w:{mainWindow.width()} h:{mainWindow.height()}")
 
-        # Iterate over width dimension
-        while mainWindow.width() > mainWindow.minimumWidth():
-            testChildVisibility()
-            mainWindow.resize(mainWindow.width() - 1, mainWindow.height())
+                # Skip QSizeGrip (Handle which can be clicked with a mouse to resize the window)
+                # as this is part of the window itself and is always out of the "canvas"
+                if isinstance(w, QtWidgets.QSizeGrip):
+                    continue
 
-        # Iterate over height dimension
-        while mainWindow.height() > mainWindow.minimumHeight():
-            testChildVisibility()
-            mainWindow.resize(mainWindow.width(), mainWindow.height() - 1)
+                self.assertTrue(pointOnMainWindow(w.mapToGlobal(QPoint(0, 0))),
+                        f"Testing top-left corner of {w.__class__.__name__} widget.")
+                self.assertTrue(pointOnMainWindow(w.mapToGlobal(QPoint(w.width(),0))),
+                        f"Testing top-right corner of {w.__class__.__name__} widget.")
+                self.assertTrue(pointOnMainWindow(w.mapToGlobal(QPoint(0, w.height()))),
+                        f"Testing bottom-left corner of {w.__class__.__name__} widget.")
+                self.assertTrue(pointOnMainWindow(w.mapToGlobal(QPoint(w.width(), w.height()))),
+                        f"Testing bottom-right corner of {w.__class__.__name__} widget.")
 
-        # Check the smallest possible size again
-        mainWindow.resize(0, 0)
-        testChildVisibility()
+        print(f"Testing widget visibility on default window size:")
+        print(f"Window width: {mainWindow.geometry().width()}")
+        print(f"Window height: {mainWindow.geometry().height()}")
+        testWidgetsIfOnMainScreen()
+
+        # Resize window to minium size
+        mainWindow.resize(0, 0) # This makes smallest possible size (respecting design constrains)
+        QtWidgets.QApplication.instance().processEvents()
+
+        print(f"Testing widget visibility on smallest possible size allowed by design:")
+        print(f"Window width: {mainWindow.geometry().width()}")
+        print(f"Window height: {mainWindow.geometry().height()}")
+        testWidgetsIfOnMainScreen()
